89f0f92a07c162e795420f35cfa7a5eb453ee60f
Ridwan <radowan.me@gmail.com>
2022-05-08 19:42:49 +0600

    products api

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config()
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware 
+
+app.use(cors())
+app.use(express.json())
+
+
+const uri = `mongodb+srv://db-spice:GPAqNg9sWfGEwhwb@spice-house0.ajwla.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+
+        const spiceCollection = client.db('spice').collection('product')
+
+        app.get('/products', async (req, res) => {
+            const query = {}
+            const cursor = spiceCollection.find(query)
+            const products = await cursor.toArray()
+            res.send(products);
+            console.log(products);
+        })
+
+        app.get('/inventory/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const product = await spiceCollection.findOne(query)
+            res.send(product)
+        })
+
+    }
+    finally {
+
+    }
+
+}
+
+run().catch(console.dir)
+
+app.get('/', (req, res) => {
+    res.send('Spice House Api is running')
+})
+
+app.listen(port, () => {
+    console.log('Spice House Server is running', port);
+})
