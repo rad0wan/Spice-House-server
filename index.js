46b034604b51328386171d70f0069ce7633d1b6e
Ridwan <radowan.me@gmail.com>
2022-05-09 10:29:46 +0600

    delete api

--- index.js
+++ index.js
@@ -35,6 +35,14 @@ async function run() {
             res.send(product)
         })
 
+        app.delete('/inventory/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const result = await spiceCollection.deleteOne(query)
+            res.send(result)
+            console.log(result);
+        })
+
     }
     finally {
 
